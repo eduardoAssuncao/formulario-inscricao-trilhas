53cea3ed2ff7b1fd52b98922438f80b6488e9ef8
eliascmendes <eliascamilo70@outlook.com>
2025-03-15 21:07:36 -0300

    delete

--- scripts/indexScript.js
+++
@@ -1,6 +0,0 @@
-// Script para a página de redirecionamento
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(function () {
-    window.location.href = './mainView.html'
-  }, 500)
-})
